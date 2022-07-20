41f53e49425990b83798110c8661b439705da4a3
Oki1 <oskar.rotar@gmail.com>
2022-07-20 21:28:29 +0200

    partly merged 3d branch into main

--- Libraries/Terminal/Headers/Terminal.hpp
+++ Libraries/Terminal/Headers/Terminal.hpp
@@ -1,24 +1,17 @@
-#pragma once
 #include <iostream>
 #include <windows.h>
+
 class Terminal {
 private:
+	COORD screenSize;
 	CONSOLE_FONT_INFOEX font;
 	char* buffer;
 	HANDLE handle;
 	int bufferSize;
 public:
-	COORD screenSize;
 	Terminal(); //class constructor
 	void PutPixel(char, COORD);
 	void PutPixel(char, short, short);
 	void ClearBuffer(char = '#');
 	void DrawFrame();
-
-	//rasterization
-private:
-    void plotLineHigh(int x0, int y0, int x1, int y1, char c);
-    void plotLineLow(int x0, int y0, int x1, int y1, char c);
-public:
-    void drawLine(int x0, int y0, int x1, int y1, char c);
 };
