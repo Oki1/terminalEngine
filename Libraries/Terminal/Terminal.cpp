08c949a10d32649c4891c3af1084332e3b1338c4
Oki1 <oskar.rotar@gmail.com>
2022-07-20 21:40:52 +0200

    merged line rasterization from 3dEngine branch

--- Libraries/Terminal/Terminal.cpp
+++ Libraries/Terminal/Terminal.cpp
@@ -36,3 +36,73 @@ void Terminal::DrawFrame() {
 	std::cout << buffer << std::flush;
 	SetConsoleCursorPosition(handle, {0,0});
 }
+//Rasterizer
+#include <cstdlib>
+
+
+void Terminal::plotLineHigh(int x0, int y0, int x1, int y1, char c) {
+    int dx = x1 - x0;
+    int dy = y1 - y0;
+    int  xi = 1;
+    if (dx < 0) {
+        xi = -1;
+        dx = -dx;
+    }
+    int D = (2 * dx) - dy;
+    int x = x0;
+
+    for(int y = y0; y < y1; y++) {
+        PutPixel(c, x, y);
+        if (D > 0) {
+            x = x + xi;
+            D = D + (2 * (dx - dy));
+        }
+        else {
+            D = D + 2*dx;
+        }
+    }
+}
+void Terminal::plotLineLow(int x0, int y0, int x1, int y1, char c)
+{
+    int dx = x1 - x0;
+    int dy = y1 - y0;
+    int  yi = 1;
+    if (dy < 0) {
+        yi = -1;
+        dy = -dy;
+    }
+    int D = (2 * dy) - dx;
+    int y = y0;
+
+    for(int x = x0; x < x1; x++) {
+        PutPixel(c, x, y);
+        if (D > 0) {
+            y = y + yi;
+            D = D + (2 * (dy - dx));
+        }
+        else {
+            D = D + 2*dy;
+        }
+    }
+}
+
+void Terminal::drawLine(int x0, int y0, int x1, int y1, char c) {
+    if (abs(y1 - y0) < abs(x1 - x0)) {
+        if (x0 > x1) {
+            plotLineLow(x1, y1, x0, y0, c);
+        }
+        else {
+            plotLineLow(x0, y0, x1, y1, c);
+
+        }
+    }
+    else{
+        if (y0 > y1) {
+            plotLineHigh (x1, y1, x0, y0, c);
+
+        } else {
+            plotLineHigh (x0, y0, x1, y1, c);
+        }
+
+    }
+}
